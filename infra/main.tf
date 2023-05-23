27251e2e09de24ec18a7739a52b8e985981ebd56
rishabkumar7 <rishabkumar7@gmail.com>
2023-05-23 19:46:12 -0400

    enabled CDN

--- infra/main.tf
+++ infra/main.tf
@@ -54,6 +54,7 @@ resource "google_compute_backend_bucket" "website" {
   name        = "examplestaticsite"
   description = "Contains hello world"
   bucket_name = google_storage_bucket.static_site.name
+  enable_cdn  = true
 }
 
 # Create HTTPS certificate
